90afcaf473b416fdb1d4b6b3e9b707fe1fae86c9
josevazf <jose.guilherme.rvf@gmail.com>
2024-06-05 15:16:08 +0100

    fade color with distance

--- src/render.c
+++ src/render.c
@@ -6,16 +6,31 @@
 /*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 17:23:26 by jrocha-v          #+#    #+#             */
-/*   Updated: 2024/06/05 14:48:27 by jrocha-v         ###   ########.fr       */
+/*   Updated: 2024/06/05 15:15:31 by jrocha-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	get_texture_pixel(t_img *img, int x, int y)
+static int	get_texture_pixel(t_img *img, int x, int y)
 {
 	return (*(unsigned int *)(img->pixel_addr + \
 		(y * img->line_len) + (x * (img->bpp / 8))));
+}
+
+static int	fade_color(int color, double factor)
+{
+	int	r;
+	int	g;
+	int	b;
+
+	factor /= 5;
+	if (factor < 1)
+		return (color);
+	r = (color >> 16) / factor;
+	g = (color >> 8 & 0xFF) / factor;
+	b = (color & 0xFF) / factor;
+	return (r << 16 | g << 8 | b);
 }
 
 void	draw_textured_wall(t_data *data, int i, int j)
@@ -25,17 +40,17 @@ void	draw_textured_wall(t_data *data, int i, int j)
 	rel_pos = ((j - data->rays[i].wall_start)
 			/ (data->rays[i].wall_end - data->rays[i].wall_start)) * 64.0;
 	if (!data->rays[i].hit_vert && data->rays[i].is_up)
-		put_pixel(&data->img, i, j, get_texture_pixel(&data->txts_img[NO],
-				(int)data->rays[i].x_hit % 64, rel_pos));
+		put_pixel(&data->img, i, j, fade_color(get_texture_pixel(&data->txts_img[NO],
+				(int)data->rays[i].x_hit % 64, rel_pos), data->rays[i].distance / 50));
 	else if (!data->rays[i].hit_vert && !data->rays[i].is_up)
-		put_pixel(&data->img, i, j, get_texture_pixel(&data->txts_img[SO],
-				(int)data->rays[i].x_hit % 64, rel_pos));
+		put_pixel(&data->img, i, j, fade_color(get_texture_pixel(&data->txts_img[SO],
+				(int)data->rays[i].x_hit % 64, rel_pos), data->rays[i].distance / 50));
 	else if (data->rays[i].hit_vert && data->rays[i].is_right)
-		put_pixel(&data->img, i, j, get_texture_pixel(&data->txts_img[EA],
-				(int)data->rays[i].y_hit % 64, rel_pos));
+		put_pixel(&data->img, i, j, fade_color(get_texture_pixel(&data->txts_img[EA],
+				(int)data->rays[i].y_hit % 64, rel_pos), data->rays[i].distance / 50));
 	else
-		put_pixel(&data->img, i, j, get_texture_pixel(&data->txts_img[WE],
-				(int)data->rays[i].y_hit % 64, rel_pos));
+		put_pixel(&data->img, i, j, fade_color(get_texture_pixel(&data->txts_img[WE],
+				(int)data->rays[i].y_hit % 64, rel_pos), data->rays[i].distance / 50));
 }
 
 void	render_walls(t_data *data, int i, int j)
